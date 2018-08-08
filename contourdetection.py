20e6de5e1853edfa8e02292b26d138404834a64d
jillantony <38492594+jillantony@users.noreply.github.com>
2018-08-08 23:36:18 +0530

    Update contourdetection.py

--- contourdetection.py
+++ contourdetection.py
@@ -1,3 +1,9 @@
 import cv2
 import numpy as np
 capture = cv2.VideoCapture(0)
+while(True):
+	ret, frame = capture.read()
+	cv2.imshow('Original',frame)
+	cv2.imshow('Contour detection',detectContours(frame))
+	if cv2.waitKey(1) == 13:
+		break
